f6f5e6b0c1e1003ac1fc5b4354d6a2b707ff61aa
ksimon17 <78188196+ksimon17@users.noreply.github.com>
2022-02-09 23:43:59 -0500

    Update shopping_cart.py
    created a while loop within shopping_cart.py

--- shopping_cart.py
+++ shopping_cart.py
@@ -42,15 +42,21 @@ def to_usd(my_price):
 # ASK FOR USER INPUT
 
 
-product_id = input("Please input a product identifier: ")
-matching_products = []
-for x in products: 
-    if str(x["id"]) == str(product_id): 
-        matching_products.append(x)
-print(matching_products)
+while True:
+
+    product_id = input("Please input a product identifier: ")
+
+    if product_id == "DONE":
+        break
+
+    matching_products = []
+    for x in products: 
+        if str(x["id"]) == str(product_id): 
+            matching_products.append(x)
+    print(matching_products)
 
 
-# Print the name of the matching product 
+    # Print the name of the matching product 
 
-matching_product = matching_products[0]
-print(matching_product["name"], matching_product["price"])
+    matching_product = matching_products[0]
+    print(matching_product["name"], matching_product["price"])
